d276b3596037278301bdd91d61481a7cdebe14ab
kapil-zagtec <kapil.zagtec@gmail.com>
2019-04-27 18:37:21 +0530

    ok

--- src/constants.js
+++ src/constants.js
@@ -4,7 +4,7 @@ const apiVersion = 'v1';
 const hostname = window && window.location && window.location.hostname;
 console.log(hostname);
 
-if(hostname === 'aryawebstudio.in') {
+if(hostname === 'scoreboard.aryawebstudio.in') {
   backendHost = 'http://scoreboardapi.aryawebstudio.in';
 } else if(hostname === 'localhost') {
   backendHost = 'http://127.0.0.1:8080';
